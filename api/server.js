b46ca762161fccb52ff4e4727dc58fe37eb729a3
Jhon Pineda <pinedajhon12@outlook.com>
2023-03-24 18:39:17 +0800

    ye

--- api/server.js
+++ api/server.js
@@ -43,5 +43,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on the port  ${3080}`);
+    console.log(`Server listening on the port  ${port}`);
 })
